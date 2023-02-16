f15bc278c1e843e91d068ed73cddead69ac0cb54
Damilare Lawal <Rasco2703@gmail.com>
2023-02-16 15:19:56 -0800

    6 Size Of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *Return: Always 0 (SUCCESS)
+ */
+
+int main(void)
+{
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of a float: %zu bytes(s)\n", sizeof(float));
+	printf("Size of a long int: %zu bytes(s)\n", sizeof(long int));
+		return (0);
+}
+	

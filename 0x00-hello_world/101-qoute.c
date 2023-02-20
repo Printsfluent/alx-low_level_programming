883ee6c1cbb810a60a4be7bf0c5c858b4a4f1ffe
Damilare Lawal <Rasco2703@gmail.com>
2023-02-19 21:21:14 -0800

    101-qoute

--- 0x00-hello_world/101-qoute.c
+++ 0x00-hello_world/101-qoute.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
- *main - entry point
- *Return: 1
+ *main - print an output
+ *Return: 1 (Error)
  */
 
 int main(void)
 {
-fwrite("and that piece of art is useful" - Dora Korpar, 2015-10-19"\n);
-		return (1);
+	write(2, "and that piece of art is useful - Dora Korpar,2015-10-19\n", 59);
+
+	return (1);
 }
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints all possible combinations of single-digit numbers.
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+	int a,b
+	

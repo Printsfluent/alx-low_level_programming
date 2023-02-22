2a9d74520467d3dc71db8c81b8ba0b6aadcbc5a9
Damilare Lawal <Rasco2703@gmail.com>
2023-02-22 00:28:06 -0800

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ *_abs - Print the absolute value of an interger
+ *@b: return the absolute valeu
+ *Return: absiolute value
+ */
+
+int _abs(int b)
+{
+int a;
+	a = abs(b);
+		return (a);
+}

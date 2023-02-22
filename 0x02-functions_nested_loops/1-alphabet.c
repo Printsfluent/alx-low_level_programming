04a0f9f69e73b614837c78b4e1eae10d60f04531
Damilare Lawal <Rasco2703@gmail.com>
2023-02-21 18:58:21 -0800

    Print Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print
+ *
+ * Description: prints_alphabet() prints the alphabet in lower case
+ *
+ */
+void print_alphabet(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+		_putchar(a);
+
+	_putchar('\n');
+}

a99e2f2a08eb5388071df079f69db4049f80ccb0
Damilare Lawal <Rasco2703@gmail.com>
2023-02-23 22:15:28 -0800

    Tasks 4 - 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - print n underscore characters
+ * @n: number of times to print _
+ */
+void print_line(int n)
+{
+	int set;
+
+	if (n > 0)
+	{
+		for (set = 0; set < n; set++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}

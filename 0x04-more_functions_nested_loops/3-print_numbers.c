eb296104764dca198a5222889056fce5725426c6
Damilare Lawal <Rasco2703@gmail.com>
2023-02-23 21:47:03 -0800

    Tasks 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_numbers - Function print numbers from 0 to 9
+ *
+ *Return: empty
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+
+}

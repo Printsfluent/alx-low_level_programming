8a4301dda6ead69497db42e163447568f2860131
Damilare Lawal <Rasco2703@gmail.com>
2023-02-27 18:24:29 -0800

    Tasks 5 -9

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,20 @@
 #include "main.h"
-#include <string.h>
 
 /**
- *rev_string - prints function in reverse
- *@s: pointer
- *
+ * rev_string - reverses a string
+ * @s: the string to be reversed
  */
 void rev_string(char *s)
-
 {
-	int len = strlen(s);
+	int len = 0, i = 0;
 	char temp;
 
-	for (int i = 0; i < len / 2; i++)
-{
-	temp = s[i];
-	s[i] = s[len - i - 1];
-	s[len - i - 1] = temp;
-}
+	while (s[len] != '\0')
+		len++;
+	while (i < len--)
+	{
+		temp = s[i];
+		s[i++] = s[len];
+		s[len] = temp;
+	}
 }
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other charcter of a string
+ * starting with the first
+ * @str: the string to be worked on
+ */
+void puts2(char *str)
+{
+	int len = 0, i = 0;
+
+	while (str[len] != '\0')
+		len++;
+
+	len = len - 1;
+
+	for (; i <= len; i += 2)
+	putchar(str[i]);
+
+	putchar('\n');
+}

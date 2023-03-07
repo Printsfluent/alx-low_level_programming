a639420de2966691856f6c10145bd7facee8a019
Damilare Lawal <Rasco2703@gmail.com>
2023-03-06 18:23:31 -0800

    memcpyu

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_memcpy - Function that copy memory area
+ *@dest: First Memory area
+ *@src: Second Memory Area
+ *@n: number of bytes
+ *Return: copied memory with n byte
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+int i;
+	for (i = 0; i < n; i++)
+	{
+	dest[i] = src[1];
+	}
+	return (dest);
+}

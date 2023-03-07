068531366422664455f293adc7d43333548449f2
Damilare Lawal <Rasco2703@gmail.com>
2023-03-06 19:14:14 -0800

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strstr - Function that locates a substring.
+ * @haystack: input
+ * @needle: Second Input
+ * Return: Always 0 (Success)
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	{
+	for (; *haystack != '\0'; haystack++)
+
+		while (*haystack == *needle && *needle != '\0')
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+	}
+
+	return (0);
+}
+

3fd0d9f387a9b5d39c488f7daa22dbeefcf85820
Damilare Lawal <Rasco2703@gmail.com>
2023-03-11 05:16:11 -0800

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *main - Entry Point
+ *@argc: argument counter
+ *@argv: argument vector
+ *Return: (0) SUCCESS
+ */
+
+int main(int argc, char *argv[])
+{
+printf("%s\n", *argv);
+
+return (0);
+}

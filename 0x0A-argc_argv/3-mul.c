c53cb48c4a634b1af82373272d67e5057b209444
Damilare Lawal <Rasco2703@gmail.com>
2023-03-11 20:10:35 -0800

    Task 4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -16,10 +16,8 @@ int main(int argc, char *argv[])
 	else
 	{
 		int result;
-		int num1 = atoi(argv[1]); 
-		int num2 = atoi(argv[2]);
 
-		result = num1  * num2;
+		result = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", result);
 	}
 	return (0);
